6bc244600544254177ce832da5b59e17eef220ed
Swar <4al22cs176@gmail.com>
2025-07-15 07:14:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (49.74%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n){
+        return fib(n,new HashMap<>());
+    }
+    public int fib(int n, HashMap<Integer,Integer> memo) {
+        if(n==1||n==0){
+            return n;
+        }
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        int result=fib(n-1,memo)+fib(n-2,memo);
+        memo.put(n,result);
+        return result;
+    }
+}
