be35da03006291ad599eaf7455bd061461d1ff82
Swar <4al22cs176@gmail.com>
2025-07-24 14:33:27 +0530

    Time: 4 ms (11.87%) | Memory: 45.9 MB (5.50%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -1,34 +1,32 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
-
-        for (int i = 0; i < mat.length; i++) {
-            int x = 0;
-            for (int j = 0; j < mat[0].length; j++) {
-                if (mat[i][j] == 1) x++;
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        HashMap<Integer,ArrayList<Integer>> hm=new HashMap<>();
+        for(int i=0;i<mat.length;i++){
+            int x=0;
+            ArrayList<Integer> arr=new ArrayList<>();
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j]==1)x++;
             }
-
-            hm.putIfAbsent(x, new ArrayList<>());
+            hm.putIfAbsent(x,new ArrayList<>());
             hm.get(x).add(i);
         }
 
-        // Add unique soldier counts (keys) to the priority queue
-        for (int key : hm.keySet()) {
+         for (int key : hm.keySet()) {
             pq.add(key);
         }
 
-        int[] arr = new int[k];
-        int i = 0;
-
-        while (k > 0) {
-            ArrayList<Integer> arrst = hm.get(pq.poll());
-            for (int j = 0; j < arrst.size() && k > 0; j++) {
-                arr[i++] = arrst.get(j);
+        int[] arr=new int[k];
+        int i=0,flag=0;
+        while(k>0){
+            ArrayList<Integer> arrst=hm.get(pq.poll());
+            for(int j=0;j<arrst.size() && k>0;j++){
+                arr[i++]=arrst.get(j);
+                
                 k--;
             }
         }
-
+        
         return arr;
     }
 }
