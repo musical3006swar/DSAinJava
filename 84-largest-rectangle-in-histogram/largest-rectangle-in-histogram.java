d54a84d3c71f0a9b8730adff50b902e9e2dbf348
Swaroop  S <4al22cs176@gmail.com>
2025-09-21 09:36:58 +0530

    Time: 113 ms (21.41%) | Memory: 58 MB (22.39%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int largestRectangleArea(int[] arr) {
+        int n=arr.length;
+        Stack<Integer> st=new Stack<>();
+        int[] leftSmall=new int[n];
+        leftSmall[0]=0;st.add(0);
+        for (int i=0; i<n; i++) {
+            while (!st.isEmpty() && arr[st.peek()]>=arr[i]) {
+                st.pop();
+            }
+ 
+            if (st.isEmpty()) leftSmall[i] = 0;
+            else leftSmall[i] = st.peek()+1;
+                st.push(i);
+        }
+        st.clear();
+        //Stack<Integer> st=new Stack<>();
+        int[] rightSmall=new int[n];
+        rightSmall[n-1]=n-1;st.add(n-1);
+        for (int i=n-1; i>=0; i--) {
+            while (!st.isEmpty() && arr[st.peek()]>=arr[i]) {
+                st.pop();
+            }
+ 
+            if (st.isEmpty()) rightSmall[i] = n-1;
+            else rightSmall[i] = st.peek()-1;
+                st.push(i);
+        }
+      
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,arr[i]*(rightSmall[i]-leftSmall[i]+1));
+        }
+        
+        // for(int i=0;i<leftSmall.length;i++)System.out.print(leftSmall[i]+" ");
+        // System.out.println();
+        // for(int i=0;i<leftSmall.length;i++)System.out.print(rightSmall[i]+" ");
+        return max;
+    }
+}
