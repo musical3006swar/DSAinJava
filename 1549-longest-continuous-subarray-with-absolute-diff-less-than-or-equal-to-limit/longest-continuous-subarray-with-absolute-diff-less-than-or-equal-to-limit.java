0aed82b4d2585768b33e835e05740f83b175e861
Swaroop  S <4al22cs176@gmail.com>
2025-09-28 14:41:22 +0530

    Time: 45 ms (34.73%) | Memory: 57.7 MB (64.47%) - LeetSync

---
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        int n=nums.length;
+        Deque<Integer> max = new ArrayDeque<>();
+        Deque<Integer> min = new ArrayDeque<>();
+        int startpos= 0; 
+        int bestStart= 0, bestLen = 0;
+        for(int i=0;i<n;i++){
+            while(!min.isEmpty() && nums[min.peekLast()]>=nums[i]){
+                min.pollLast();
+            }
+            min.addLast(i);
+            while(!max.isEmpty() && nums[max.peekLast()]<=nums[i]){
+                max.pollLast();
+            }
+            max.addLast(i);
+            while(!min.isEmpty() && !max.isEmpty() && nums[max.peekFirst()]-nums[min.peekFirst()]>limit){
+                if(min.peekFirst()==startpos)min.pollFirst();
+                if(max.peekFirst()==startpos)max.pollFirst();
+                startpos++;
+
+            }
+            if(i-startpos+1>bestLen){
+                bestLen=i-startpos+1;
+                bestStart=startpos;
+            }
+
+        }
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=bestStart;i<bestStart+bestLen;i++){
+            arr.add(nums[i]);
+        }
+        return bestLen;
+    }
+}
