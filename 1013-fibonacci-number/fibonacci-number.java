df144c49496fd2ef972b5a582bca35a10dfe6a38
Swaroop  S <4al22cs176@gmail.com>
2025-10-01 08:37:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (81.91%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -3,15 +3,15 @@ class Solution {
         return fib(n,new HashMap<>());
     }
     public int fib(int n, HashMap<Integer,Integer> memo) {
+        
         if(n==1||n==0){
             return n;
         }
-        // if(memo.containsKey(n)){
-        //     return memo.get(n);
-        // }
-        // int result=fib(n-1,memo)+fib(n-2,memo);
-        // memo.put(n,result);
-        // return result;
-        return fib(n-1)+fib(n-2);
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        int result=fib(n-1,memo)+fib(n-2,memo);
+        memo.put(n,result);
+        return result;
     }
 }
