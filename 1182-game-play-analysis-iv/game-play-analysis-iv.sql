2a5d1f6125d075b319e4ca8466ca7879c680e7b0
Swaroop  S <4al22cs176@gmail.com>
2025-11-30 23:50:09 +0530

    Time: 507 ms (81.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,3 @@
+select round(sum(df)/count(distinct(player_id)),2) as fraction
+from (select player_id, datediff(event_date,min(event_date) over(partition by player_id))=1 as df
+from Activity) as nt;
