9882914c74d5553f2e9aece4105e41e4596eeb53
Swaroop  S <4al22cs176@gmail.com>
2026-01-30 11:09:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (11.88%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,18 +1,17 @@
 class Solution {
     public int climbStairs(int n){
-        return climbStairs(n,new HashMap<>());
+        return climbStairs(n, new HashMap<>());
     }
-    public int climbStairs(int n, HashMap<Integer,Integer> memo) {
-        
+    public int climbStairs(int n, HashMap<Integer,Integer> hm){
         if(n==1||n==2){
-            memo.put(n,n);
+            hm.put(n,n);
             return n;
         }
-        if(memo.containsKey(n)){
-            return memo.get(n);
+        if(hm.containsKey(n)){
+            return hm.get(n);
         }
-        int result=climbStairs(n-1,memo)+climbStairs(n-2,memo);
-        memo.put(n,result);
+        int result=climbStairs(n-1, hm)+climbStairs(n-2,hm);
+        hm.put(n,result);
         return result;
     }
 }
