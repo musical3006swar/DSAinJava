910434263de2603217365dcd27ae5ce8deb60d44
Swaroop  S <4al22cs176@gmail.com>
2026-03-30 15:55:32 +0530

    Time: 809 ms (63.15%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,5 +1,6 @@
-delete q
-from  Person p
-join Person q
-on p.email=q.email
-and p.id<q.id;
+# Write your MySQL query statement below
+delete p1
+from Person p1
+join Person p2
+on p1.email=p2.email
+and p1.id>p2.id;
