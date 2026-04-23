6a61f3c2332e8b8cc81234b9774441d33030725a
Swaroop  S <4al22cs176@gmail.com>
2026-04-23 09:52:23 +0530

    Time: 6 ms (19.65%) | Memory: 51.4 MB (87.55%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,26 +1,54 @@
-class Solution {
-    
-    public static void zerodfs(int r, int c,char[][] grid){
-        if(r<0||r>=grid.length||c<0||c>=grid[0].length||grid[r][c]=='0')return;
-        grid[r][c]='0';
-        zerodfs(r-1,c,grid);
-        zerodfs(r+1,c,grid);
-        zerodfs(r,c-1,grid);
-        zerodfs(r,c+1,grid);
+class Pair{
+    int col;
+    int row;
+    public Pair(int col, int row){
+        this.col=col;
+        this.row=row;
     }
-    
+}
+
+
+class Solution {
+    public void numIslands(char[][] grid, int[][] vis, int i, int j, int m, int n){
+        vis[i][j]=1;
+        Queue<Pair> que= new LinkedList<>();
+        que.add(new Pair(i,j));
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, 1, 0, -1};
+        while(!que.isEmpty()){
+            int colIdx=que.peek().col;
+            int rowIdx=que.peek().row;
+            que.poll();
+            for(int k=0;k<4;k++){
+                    int newColIdx = colIdx + dRow[k];
+                    int newRowIdx = rowIdx + dCol[k];
+                    if( 
+                        newColIdx<m && newColIdx>=0
+                        && newRowIdx<n && newRowIdx>=0
+                        && grid[newColIdx][newRowIdx]=='1' 
+                        && vis[newColIdx][newRowIdx]==0
+                        
+                        ){
+                        que.add(new Pair(newColIdx, newRowIdx));
+                        vis[newColIdx][newRowIdx]=1;
+                    } 
+            }
+            
+        }
+    }
     public int numIslands(char[][] grid) {
-        int island=0;
-        int r=grid.length;
-        int c=grid[0].length;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(grid[i][j]=='1'){
-                    zerodfs(i,j,grid);
-                    island++;
+        int m=grid.length;
+        int n=grid[0].length;
+        int numOfIslands=0;
+        int[][] vis= new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    numOfIslands++;
+                    numIslands(grid,vis,i,j,m,n);
                 }
             }
         }
-        return island;
+        return numOfIslands;
     }
 }
