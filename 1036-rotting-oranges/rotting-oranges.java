09024cd24d8ab36818ea33ebc5405af5ad1e5ded
Swaroop  S <4al22cs176@gmail.com>
2026-04-24 10:37:10 +0530

    Time: 2 ms (90.34%) | Memory: 44.1 MB (57.92%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -14,7 +14,7 @@ class Solution {
         Queue<Pair> que = new LinkedList<>();
         int[] delRow = {-1,0,1,0};
         int[] delCol = {0,-1,0,1};
-        int currTime=0,fresh=0;
+        int currTime=0,fresh=0,timeTaken=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(copy[i][j]==2){
@@ -26,34 +26,30 @@ class Solution {
         }
         if(fresh==0)return 0;
         while(!que.isEmpty()){
-            boolean spread = false;
-            
-                // int size = queA.size();
-                // for(int s=0;s<size;s++){
-                    Pair p = que.poll();
-
-                    for(int k=0;k<4;k++){
-                        int newLeft = p.left + delRow[k];
-                        int newRight = p.right + delCol[k];
-                        currTime = p.time+1;
-                        if(
-                            newLeft >= 0 && newLeft < m &&
-                            newRight >= 0 && newRight < n &&
-                            copy[newLeft][newRight] == 1 &&
-                            vis[newLeft][newRight] == 0 
-                        ){
-                            copy[newLeft][newRight] = 2; 
-                            vis[newLeft][newRight] = 1;
-                            que.add(new Pair(newLeft, newRight,currTime));   
-                            fresh--;
-                            spread=true;                     
-                        }
-                    }
+            boolean spread = false;        
+            Pair p = que.poll();
+            for(int k=0;k<4;k++){
+                int newLeft = p.left + delRow[k];
+                int newRight = p.right + delCol[k];
+                currTime = p.time+1;
+                if(
+                    newLeft >= 0 && newLeft < m &&
+                    newRight >= 0 && newRight < n &&
+                    copy[newLeft][newRight] == 1 &&
+                    vis[newLeft][newRight] == 0 
+                ){
+                    copy[newLeft][newRight] = 2; 
+                    vis[newLeft][newRight] = 1;
+                    que.add(new Pair(newLeft, newRight,currTime));   
+                    fresh--;
+                    spread=true;                     
+                }
+            }
             
         }
         if(fresh>0)return -1;
-        return currTime-1;
-
+        timeTaken=currTime-1;
+        return timeTaken;
     }
     public int orangesRotting(int[][] grid) {
         int[][] copy = grid;
